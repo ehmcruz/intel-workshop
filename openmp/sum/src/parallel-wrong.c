4b4634ec51cec5e6b0c9744a5fc4afee5ee53e1f
Eduardo H. M. Cruz <eduardohmdacruz@gmail.com>
2016-09-05 10:04:13 -0300

    v

---
+++ openmp/sum/src/parallel-wrong.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int sum(int *v, int n)
+{
+	int i, sum, nthreads;
+	
+	printf("parallel\n");
+	
+	sum = 0;
+	
+	#pragma omp parallel shared(sum, nthreads) private(i)
+	{
+		#pragma omp single
+		{
+			nthreads = omp_get_num_threads();
+		}
+		
+		#pragma omp flush(nthreads)
+		
+		for (i=omp_get_thread_num(); i<n; i+=nthreads) {
+			sum += v[i];
+		}
+	}
+	
+	return sum;
+}
