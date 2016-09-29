d4c0d5ae085c4315f3da5ca04790835978f035f2
Eduardo Cruz <eduardohmdacruz@gmail.com>
2016-09-29 11:27:55 -0300

    avx matrix mult

---
+++ openmp-xeonphi/matrix-mult/src/seq-simd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#include "matrix.h"
+
+void matrix_mult (myfloat *first, myfloat *second, myfloat *multiply, int first_rows, int first_cols, int second_cols)
+{
+	int i, j, k;
+	myfloat sum;
+	
+	for (i=0; i<first_rows; i++) {
+		for (j=0; j<second_cols; j++) {
+			sum = 0;
+			
+			#pragma simd reduction(+:sum)
+			for (k=0; k<first_cols; k++) {
+				sum += first[i*first_cols+k] * second[k*second_cols+j];
+			}
+
+			multiply[i*second_cols+j] = sum;
+		}
+	}
+}
